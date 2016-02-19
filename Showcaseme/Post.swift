ed24a916685f05b17247254333942e4350ba93af
Adriana Gonzalez <adrianagonzalez@its-mac-mini-3.mty.ipointsystems.com>
2016-02-19 17:38:36 -0600

    Using imageshack, still in progress.

---
+++ Showcaseme/Post.swift
@@ -0,0 +1,9 @@
+//
+//  Post.swift
+//  Showcaseme
+//
+//  Created by Adriana Gonzalez on 2/19/16.
+//  Copyright © 2016 Adriana Gonzalez. All rights reserved.
+//
+
+import Foundation
---
+++ Showcaseme/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  Showcaseme
+//
+//  Created by Adriana Gonzalez on 2/18/16.
+//  Copyright © 2016 Adriana Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
